11b0525cafb839d0dada921817c106cea647c265
Parzival <srivichai95@gmail.com>
2025-10-01 01:01:49 +0700

    3.1 POST enrollment (fix bodyParse)

--- src/routes/enrollments_v2.ts
+++ src/routes/enrollments_v2.ts
@@ -8,7 +8,7 @@ import type { Enrollment, CustomRequest, UserPayload, Student } from "../libs/ty
 // import database
 import { enrollments, reset_enrollments, students } from "../db/db.js";
 import { success } from "zod";
-import { zStudentId } from "../libs/zodValidators.js"
+import { zStudentId,zEnrollmentBody } from "../libs/zodValidators.js"
 import { authenticateToken } from "../middlewares/authenMiddleware.js";
 import { checkRoleAdmin } from "../middlewares/checkRoleAdminMiddleware.js";
 import { checkRoles } from "../middlewares/checkRolesMiddleware.js"
@@ -87,6 +87,80 @@ router.get(
 
 // POST student enroll
 router.post(
+    "/:studenId",
+    authenticateToken, // verify token and extract "user payload"
+    checkRoles,
+    (req: Request, res: Response) => {
+        try {
+            const body = req.body as Enrollment;
+            const bodyParseResult = zEnrollmentBody.safeParse(body);
+            if (!bodyParseResult.success) {
+                return res.status(400).json({
+                    message: "Validation failed",
+                    errors: bodyParseResult.error.issues[0]?.message,
+                });
+            }
+
+            const payload = (req as CustomRequest).user;
+            const studentId = req.params.studenId;
+            const parseResult = zStudentId.safeParse(studentId);
+            if (!parseResult.success) {
+                return res.status(400).json({
+                    message: "Validation failed",
+                    errors: parseResult.error.issues[0]?.message,
+                });
+            }
+
+            if (payload?.role === "ADMIN" && payload?.studentId !== studentId) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Forbidden access",
+                });
+            }
+
+            const foundIndex = students.findIndex(
+                (s: Student) => s.studentId === studentId
+            );
+            if (foundIndex === -1) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Student does not exists",
+                });
+            }
+
+            // check if course id exists
+            if (students[foundIndex]?.courses?.includes(body.courseId)) {
+                return res.status(400).json({
+                    success: false,
+                    message: "studentId && courseId already exists",
+                });
+            }
+
+            students[foundIndex]?.courses?.push(body.courseId);
+
+            const enrollement: Enrollment = {
+                studentId: String(studentId),
+                courseId: body.courseId,
+            };
+            enrollments.push(enrollement);
+
+            return res.status(201).json({
+                success: true,
+                message: `Student ${studentId} && Course ${body.courseId} has been added successfully`,
+                data: enrollement,
+            });
+        } catch (err) {
+            return res.status(200).json({
+                success: false,
+                message: "Something is wrong, please try again",
+                error: err,
+            });
+        }
+    }
+);
+
+// DELETE student enroll
+router.delete(
     "/:studenId",
     authenticateToken, // verify token and extract "user payload"
     checkRoles,
