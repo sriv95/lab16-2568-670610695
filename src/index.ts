3397a7476e8ed8714f0191f9bed44d31a3519b6a
Parzival <srivichai95@gmail.com>
2025-09-30 23:43:43 +0700

    token student

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import notFoundMiddleware from "./middlewares/notFoundMiddleware.js";
 import studentRouter_v2 from "./routes/studentsRoutes_v2.js";
 import studentRouter_v3 from "./routes/studentsRoutes_v3.js";
 import courseRouter_v2 from "./routes/coursesRouters_v2.js";
+import userRouter_v2 from "./routes/usersRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -25,7 +26,7 @@ app.use(invalidJsonMiddleware);
 
 // Endpoints
 app.get("/", (req: Request, res: Response) => {
-  res.send("Lecture18 API services");
+  res.send("Lab 16 API services");
 });
 
 app.get("/me", (req: Request, res: Response) => {
@@ -45,6 +46,7 @@ app.get("/me", (req: Request, res: Response) => {
 app.use("/api/v2/students", studentRouter_v2);
 app.use("/api/v3/students", studentRouter_v3);
 app.use("/api/v2/courses", courseRouter_v2);
+app.use("/api/v2/users", userRouter_v2);
 
 // endpoint check middleware
 app.use(notFoundMiddleware);
---
+++ src/routes/usersRoutes.ts
@@ -0,0 +1,162 @@
+import { Router, type Request, type Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+import type { User, CustomRequest, UserPayload } from "../libs/types.js";
+
+// import database
+import { users, reset_users, students } from "../db/db.js";
+import { success } from "zod";
+
+const router = Router();
+
+// GET /api/v2/users
+router.get("/", (req: Request, res: Response) => {
+    try {
+        const authHeader = req.headers["authorization"]
+
+        if (!authHeader || !authHeader.startsWith("Bearer")) {
+            return res.status(401).json({
+                success: false,
+                message: "Authorization header is not found"
+            })
+        }
+
+        const token = authHeader?.split(" ")[1]
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Token is required"
+            })
+        }
+
+        try {
+            const jwt_secret = process.env.JWT_SECRET || "forgot_secret"
+            jwt.verify(token, jwt_secret, (err, payload) => {
+                if (err) {
+                    return res.status(403).json({
+                        success: false,
+                        message: "Invalid or expired token"
+                    })
+                }
+
+                const user = users.find(
+                    (u: User) => u.username === (payload as UserPayload).username
+                )
+
+                if (!user || user.role !== "ADMIN") {
+                    return res.status(401).json({
+                        success: false,
+                        message: "Unauthorized user"
+                    })
+                }
+
+                // return all users
+                return res.json({
+                    success: true,
+                    message: "Successful operation",
+                    data: users,
+                });
+            })
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "Something is wrong, please try again",
+                error: err,
+            })
+        }
+    } catch (err) {
+        return res.status(200).json({
+            success: false,
+            message: "Something is wrong, please try again",
+            error: err,
+        });
+    }
+});
+
+// POST /api/v2/users/login
+router.post("/login", (req: Request, res: Response) => {
+    // 1. get username and password from body
+    try {
+        const { username, password } = req.body;
+
+        const user = users.find(
+            (u: User) => u.username === username
+                && u.password === password
+        )
+        // 2. check if user exists (search with username & password in DB)
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid username or password"
+            })
+        }
+        // 3. create JWT token (with user info object as payload) using JWT_SECRET_KEY
+        //    (optional: save the token as part of User data)
+        const jwt_secret = process.env.JWT_SECRET || "forgot_secret"
+        const token = jwt.sign({
+            username: user.username,
+            role: user.role,
+            studentId: user.studentId,
+        }, jwt_secret, { expiresIn: "5m" });
+        // 4. send HTTP response with JWT token
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            token
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong.",
+            error: err
+        })
+    }
+
+
+
+    return res.status(500).json({
+        success: false,
+        message: "POST /api/v2/users/login has not been implemented yet",
+    });
+});
+
+// POST /api/v2/users/logout
+router.post("/logout", (req: Request, res: Response) => {
+    // 1. check Request if "authorization" header exists
+    //    and container "Bearer ...JWT-Token..."
+
+    // 2. extract the "...JWT-Token..." if available
+
+    // 3. verify token using JWT_SECRET_KEY and get payload (username, studentId and role)
+
+    // 4. check if user exists (search with username)
+
+    // 5. proceed with logout process and return HTTP response
+    //    (optional: remove the token from User data)
+
+    return res.status(500).json({
+        success: false,
+        message: "POST /api/v2/users/logout has not been implemented yet",
+    });
+});
+
+// POST /api/v2/users/reset
+router.post("/reset", (req: Request, res: Response) => {
+    try {
+        reset_users();
+        return res.status(200).json({
+            success: true,
+            message: "User database has been reset",
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Something is wrong, please try again",
+            error: err,
+        });
+    }
+});
+
+export default router;
