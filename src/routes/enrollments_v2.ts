2cc60e10c49ff3d34b7984c05e0d03f8cfa96b4f
Parzival <srivichai95@gmail.com>
2025-10-01 00:56:34 +0700

    2.1 GET student enrollments by admin or student

--- src/routes/enrollments_v2.ts
+++ src/routes/enrollments_v2.ts
@@ -3,10 +3,15 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
+import type { Enrollment, CustomRequest, UserPayload, Student } from "../libs/types.js";
+
 // import database
-import { enrollments, reset_enrollments } from "../db/db.js";
+import { enrollments, reset_enrollments, students } from "../db/db.js";
+import { success } from "zod";
+import { zStudentId } from "../libs/zodValidators.js"
 import { authenticateToken } from "../middlewares/authenMiddleware.js";
 import { checkRoleAdmin } from "../middlewares/checkRoleAdminMiddleware.js";
+import { checkRoles } from "../middlewares/checkRolesMiddleware.js"
 
 const router = Router();
 
@@ -20,6 +25,55 @@ router.get(
             return res.json({
                 success: true,
                 data: enrollments,
+            });
+        } catch (err) {
+            return res.status(200).json({
+                success: false,
+                message: "Something is wrong, please try again",
+                error: err,
+            });
+        }
+    }
+);
+
+// GET student enrollments by admin or student
+router.get(
+    "/:studenId",
+    authenticateToken, // verify token and extract "user payload"
+    checkRoles, // check User exists and ADMIN role
+    (req: Request, res: Response) => {
+        try {
+            const payload = (req as CustomRequest).user;
+            const studentId = req.params.studenId;
+            const parseResult = zStudentId.safeParse(studentId);
+            if (!parseResult.success) {
+                return res.status(400).json({
+                    message: "Validation failed",
+                    errors: parseResult.error.issues[0]?.message,
+                });
+            }
+
+            const foundIndex = students.findIndex(
+                (enr: Student) => enr.studentId === studentId
+            );
+            if (foundIndex === -1) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Student does not exists",
+                });
+            }
+
+            if (payload?.role === "STUDENT" && payload?.studentId !== studentId) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Forbidden access",
+                });
+            }
+
+            return res.json({
+                success: true,
+                message: "Student Information",
+                data: students[foundIndex],
             });
         } catch (err) {
             return res.status(200).json({
